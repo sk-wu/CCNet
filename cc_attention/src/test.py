c0bbc47e113c623ac624affe8d88056663e586a8
sk-wu <52479006+sk-wu@users.noreply.github.com>
2021-10-18 22:48:40 +0800

    Update test.py

--- cc_attention/src/test.py
+++ cc_attention/src/test.py
@@ -1,18 +1,23 @@
-if (x < width && y < height && z < height + width - 1) {
+__global__ void ca_forward_kernel(const float *t, const float *f, float *weight, int num, int chn, int height, int width) {
+  int x = blockIdx.x * blockDim.x + threadIdx.x;
+  int y = blockIdx.y * blockDim.y + threadIdx.y;
+  int sp = height * width;
+  int len = height + width - 1;
+  int z = blockIdx.z;
+  if (x < width && y < height && z < height + width - 1) {
 	for (int batch = 0; batch < num; ++batch) {
 		for (int plane = 0; plane < chn; ++plane) {
 			float _t = 0;
-			# 对于每个点，将水平和垂直方向上对应的通道值累加
+			# 对于(x,y)点，将水平和垂直方向上(h+w-1)个点对应的通道值累加（方便计算每个点和其他所有点的关联）
 			for (int temp_x = 0; temp_x < width; ++temp_x) {
 				_t += t[(batch * chn + plane) * sp + y * width + temp_x];
 			}
 			for (int temp_y = 0; temp_y < height; ++temp_y) {
 				_t += t[(batch * chn + plane) * sp + temp_y * width + x];
 			}
-			temp = 0;
 			if (z < width) {
 				int i = z;
-				float _f = f[(batch * chn + plane) * sp + y * width + i];
+				float _f = f[(batch * chn + plane) * sp + y * width + i]; #取(x,y)关联的第i个点通道上的值
 				weight[(batch * len + i) * sp + y * width + x] += _t * _f;
 			}
 			else {
@@ -23,6 +28,7 @@ if (x < width && y < height && z < height + width - 1) {
 			}
 		}
 	}
+  }
 }
 
 # 原文：计算每个点和十字交叉方向点的关联，用softmax计算权重。
